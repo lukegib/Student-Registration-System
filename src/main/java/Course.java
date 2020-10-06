ba0dfa2ed0d4ee03fdd78769effa5748e66e7322
Luke Gibbons <32064971+lukegib@users.noreply.github.com>
2020-10-06 16:57:06 +0100

    Added Start and End dates

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,13 +1,39 @@
+import java.time.LocalDate;
+
 public class Course {
 
     private String name;
     private String id;
     private String coordinator;
 
-    public Course(String name, String id, String coordinator) {
+    private LocalDate start;
+    private LocalDate end;
+
+    public Course(String name, String id, String coordinator, int startDay, int startMonth, int duration) {
         this.name = name;
         this.id = id;
         this.coordinator = coordinator;
+
+        int thisYear = LocalDate.now().getYear();
+
+        start = LocalDate.of(thisYear, startMonth, startDay);
+        end = start.plusMonths(duration);
+    }
+
+    public LocalDate getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDate start) {
+        this.start = start;
+    }
+
+    public LocalDate getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalDate end) {
+        this.end = end;
     }
 
     public String getName() {
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,13 +2,12 @@ public class Test {
 
     public static void main(String[] args) {
 
-        Course c = new Course("Test", "t101", "Luke");
+        Course c = new Course("Test", "t101", "Luke", 1, 9, 12);
         Student s = new Student("Luke", "Luke1", 23);
         Module m = new Module("Test subject", "t102", "Luke ii");
 
-        System.out.println(c.getName());
-        System.out.println(s.getUsername());
-        System.out.println(m.getTeacher());
+        System.out.println(c.getStart());
+        System.out.println(c.getEnd());
 
     }
 }
